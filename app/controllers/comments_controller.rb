9e65e9af16e4f0bbdd081d41f67733942d85b55c
valentinerutto <rutto28@gmail.com>
2015-08-24 17:43:31 +0300

    create action to create and save comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+      def create
+          @article = Article.find(params[:article_id]) #gets article in qsn
+          @comment = @article.comments.create(comment_params)#creates commetn
+          redirect_to article_path(@article)#redirects user to original article
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
