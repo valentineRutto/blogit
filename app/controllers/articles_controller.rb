7f1ec47f73c5cf2290be2ca1a49cb2efe2fb614f
valentinerutto <rutto28@gmail.com>
2015-08-24 11:50:31 +0300

    update action for updating existing record

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,16 @@ end
           end
       end
     
+    def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+    
     private
   def article_params
     params.require(:article).permit(:title, :text)
