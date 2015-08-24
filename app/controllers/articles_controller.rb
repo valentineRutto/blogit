ff1eefcb71c51ba398242d641994c1ed43b4732e
valentinerutto <rutto28@gmail.com>
2015-08-24 07:57:40 +0300

    checking to see how the form is sent as params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,6 @@ class ArticlesController < ApplicationController
   end
     
       def create
-            render plain: params[:article].inspect
+#            render plain: params[:article].inspect
   end
 end
